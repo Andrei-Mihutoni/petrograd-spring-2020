87a11de642ee8c1689a79ed80606bae321af306b
Andrei-Mihutoni <andrei.mihutoni@gmail.com>
2020-02-07 13:20:36 +0100

    description content changed

--- main.js
+++ main.js
@@ -18,23 +18,41 @@ function showData(jsonData) {
 
 
 function addCourses(course) {
-    console.log(course.id)
+    console.log(course)
 
     const sections = [];
     sections[0] = document.querySelector("#starters");
     sections[1] = document.querySelector("#drinks");
 
-    const template = document.querySelector("template").content;
+//    1 grab the template
+    const template = document.querySelector("#courseTemplate").content;
+
+//    2 make a copy
     const courseClone = template.cloneNode(true);
 
+//    3 change some content
     courseClone.querySelector("h3").textContent = course.name;
 
+//    chsnge the soldout
+    if(course.soldout==true){
+    courseClone.querySelector(".soldout").style.display = "block"
+        console.log('soldout')
+    }
+
+//    change the info content
+
+    courseClone.querySelector(".info p").textContent = course.shortdescription;
+
+
+//     images syntaxes from API
     const imageName = course.image;
     const base = "https://kea-alt-del.dk/t5/site/imgs/";
     const smallImg = base + "small/" + imageName + "-sm.jpg";
 
     courseClone.querySelector("img").src = smallImg;
 
+
+//    4 append the clone
     sections[0].appendChild(courseClone);
 }
 
